9445c7507c47e061409462ad32ced250d7708ab1
max <max@mcpty.com>
2026-03-21 09:58:58 +0800

    feat(database): 集成MyBatis-Plus数据库访问框架
    - 添加Spring Boot JDBC starter依赖
    - 集成MySQL数据库连接器
    - 引入MyBatis-Plus Spring Boot 3 starter
    - 在启动类中配置Mapper扫描注解
    - 添加数据源测试验证功能
    - 配置application.yml数据库连接参数
    - 设置MyBatis-Plus映射文件路径和实体包名

--- src/main/java/com/test/tms/TmsApplication.java
+++ src/main/java/com/test/tms/TmsApplication.java
@@ -1,8 +1,10 @@
 package com.test.tms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.test.tms.mapper")
 @SpringBootApplication
 public class TmsApplication {
 
--- src/test/java/com/test/tms/TmsApplicationTests.java
+++ src/test/java/com/test/tms/TmsApplicationTests.java
@@ -1,13 +1,21 @@
 package com.test.tms;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class TmsApplicationTests {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     void contextLoads() {
+        Assertions.assertNotNull(dataSource, "DataSource should be created");
     }
 
 }
