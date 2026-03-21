327a4d619fbff86adec87007c5342f9d281e3346
max <max@mcpty.com>
2026-03-21 17:38:50 +0800

    feat(tms): 删除组织机构相关实体和业务逻辑
    - 移除 Organization 实体类及其数据库映射配置
    - 删除 OrganizationMapper 数据访问接口
    - 移除 OrganizationService 业务服务接口
    - 删除 OrganizationServiceImpl 业务服务实现类
    - 清理相关的 MyBatis Plus 注解和配置
    - 完全移除组织机构模块的功能支持

--- src/main/java/com/test/tms/mapper/OrganizationMapper.java
+++
@@ -1,8 +0,0 @@
-package com.test.tms.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.test.tms.entity.Organization;
-
-public interface OrganizationMapper extends BaseMapper<Organization> {
-}
-
--- src/main/java/com/test/tms/service/OrganizationService.java
+++
@@ -1,8 +0,0 @@
-package com.test.tms.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.test.tms.entity.Organization;
-
-public interface OrganizationService extends IService<Organization> {
-}
-
